2472813a58f87580929e4cf9adcacaf4efeba9bd
Taylor <bKuzzcs@gmail.com>
2022-07-16 17:32:39 -0600

    commit tree 6766b69bbe0309271e4443048d02a3367ac19262

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,7 +6,8 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood]
+    setFoods(newFoodArray)
   }
 
   const foodList = foods.map((food) => (
